8fd6235c1eaeb4b74a7d62061a958f2adb99e65e
Satheesh Kumar <satheesh.101097@gmail.com>
2020-04-16 18:21:47 +0530

    makes content changes to work properly in pypi.org

--- setup.py
+++ setup.py
@@ -17,11 +17,12 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="iaBot",
-    version="0.0.1",
+    version="0.0.3",
     author="Satheesh Kumar",
     author_email="satheesh.101097@gmail.com",
-    description="A mini framework for building bots using python which can help you in automating your tasks",
+    description="A mini framework for building bots using python and can be served using different ways.",
     long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/satheesh1997/iaBot",
     packages=["iabot"],
     install_requires=requirements,
