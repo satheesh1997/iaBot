79fba7f8d29a02f13f8c7e88b312ac362cc95002
Suresh Kumar D <mail.sk1999@gmail.com>
2020-04-17 22:13:50 +0530

    servers: adds websocket to the list of servers (#19)
    * servers: adds basic websocket implementation
    
    * servers: fixes for graceful exit / shutdown

--- iabot/servers.py
+++ iabot/servers.py
@@ -1,8 +1,13 @@
 from abc import ABC, abstractmethod
+import asyncio
+import json
+import signal
+import websockets
 
 from flask import Flask
 
 from iabot.actions import FlaskEndPointAction
+from iabot.constants import ACTION_ERROR
 
 
 class AbstractServer(ABC):
@@ -25,3 +30,30 @@ class HTTPServer(AbstractServer):
 
     def run(self):
         self.app.run(port=self.port)
+
+
+class WebsocketServer(AbstractServer):
+    def __init__(self, bot, port=8000):
+        self.bot = bot
+        self.loop = asyncio.get_event_loop()
+        self.port = port
+
+    async def create_server(self, stop):
+        async with websockets.serve(self.echo, "0.0.0.0", self.port):
+            await stop
+
+    async def echo(self, websocket):
+        async for message in websocket:
+            try:
+                response_load = self.bot.on_message(json.loads(message))
+            except json.decoder.JSONDecodeError as e:
+                response_load = {"status": ACTION_ERROR, "message": str(e)}
+            await websocket.send(json.dumps(response_load))
+
+    def run(self):
+        stop = self.loop.create_future()
+        try:
+            self.loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+        except NotImplementedError:
+            pass
+        self.loop.run_until_complete(self.create_server(stop))
